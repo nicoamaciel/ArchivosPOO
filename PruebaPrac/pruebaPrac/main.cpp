65debcb3dfd763243ec234c0b66b8fb5962cd4b6
nicoamaciel <77804530+nicoamaciel@users.noreply.github.com>
2022-05-10 17:05:24 -0300

    Add files via upload

---
+++ PruebaPrac/pruebaPrac/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+#include "clase.h"
+
+
+
+
+
+
+
+
+int main()
+{
+    Empleado obj;
+    obj.cargar();
+    obj.mostrar();
+
+
+    return 0;
+}
